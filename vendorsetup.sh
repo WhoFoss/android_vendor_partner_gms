742564a953b26129c9294f473625266a35b5fd3b
WhoFoss <willscheyler@gmail.com>
2026-05-25 17:24:33 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,36 +13,9 @@ download_apk() {
     destination_apk="$component_name"/"$component_name".apk
     if [ -f "$destination_apk" ]; then
         echo "$destination_apk exists: not downloading"
-        ## To Do
-        # Deal with the situation where we have an OLDER version hanging around
-        # may have to be handled in the Docker image
     else
-        # echo "downloading $source_apk to $destination_apk"
         curl -L --output "$destination_apk" "$source_apk"
     fi
-}
-
-get-fdroid-components() {
-    local fdroid_repo="https://f-droid.org/repo/"
-    local name apk_to_download versioncode id
-
-    # F-Droid client app
-    name="FDroid"
-    versioncode=$(cat "$name"/.version_code)
-    id="org.fdroid.fdroid"
-    apk_to_download="$fdroid_repo"/"$id"_"$versioncode".apk
-
-    # echo "$name apk_to_download: $apk_to_download"
-    download_apk "$apk_to_download" "$name"
-
-    # FDroid Privileged Extension
-    name="FDroidPrivilegedExtension"
-    versioncode=$(cat "$name"/.version_code)
-    id="org.fdroid.fdroid.privileged"
-    apk_to_download="$fdroid_repo"/"$id"_"$versioncode".apk
-
-    # echo "$name apk_to_download: $apk_to_download"
-    download_apk "$apk_to_download" "$name"
 }
 
 get-microg-components() {
@@ -55,7 +28,6 @@ get-microg-components() {
     versioncode=$(cat "$name"/.version_code)
     id="com.google.android.gms"
     apk_to_download="$microg_repo_base"/GMSCore/releases/download/"$microg_release"/"$id"-"$versioncode".apk
-    # echo "$name apk_to_download: $apk_to_download"
     download_apk "$apk_to_download" "$name"
 
     # FakeStore
@@ -63,21 +35,17 @@ get-microg-components() {
     versioncode=$(cat "$name"/.version_code)
     id="com.android.vending"
     apk_to_download="$microg_repo_base"/GMSCore/releases/download/"$microg_release"/"$id"-"$versioncode".apk
-    # echo "$name apk_to_download: $apk_to_download"
     download_apk "$apk_to_download" "$name"
 
-    # GsfProxy the file we want is
-    #`https://github.com/microg/android_packages_apps_GsfProxy/releases/download/v0.1.0/GsfProxy.apk`
+    # GsfProxy
     name="GsfProxy"
     versioncode=$(cat "$name"/.version_code)
     apk_to_download="$microg_repo_base"/android_packages_apps_GsfProxy/releases/download/"$versioncode"/"$name".apk
-    # echo "$name apk_to_download: $apk_to_download"
     download_apk "$apk_to_download" "$name"
 }
 
-# This script is called from the root dierctory, so we need to cd
+# This script is called from the root directory, so we need to cd
 cd vendor/partner_gms
-get-fdroid-components
 get-microg-components
 # and back to the root directory
 cd ../..
